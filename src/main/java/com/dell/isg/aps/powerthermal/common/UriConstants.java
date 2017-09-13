93a0faa937e54ab82a83a30d67bc5a4b6307312d
Muhammad,Rahman <Rahman_Muhammad@dell.com>
2017-09-13 14:29:33 -0500

    RAC-5890 , Powerthermal - Version - Endpoint location needs to be refactored

--- src/main/java/com/dell/isg/aps/powerthermal/common/UriConstants.java
+++ src/main/java/com/dell/isg/aps/powerthermal/common/UriConstants.java
@@ -12,9 +12,9 @@ package com.dell.isg.aps.powerthermal.common;
  */
 public class UriConstants {
 
-    public final static String URI = "/api/1.0";
+    public final static String URI = "/api/1.0/server/powerthermal";
     public final static String VERSION = URI + "/version";
-    public final static String POWER_THERMAL = URI + "/powerthermal";
-    public final static String POWER_THERMAL_ALL = URI + "/powerthermal/all";
+    public final static String POWER_THERMAL = URI + "";
+    public final static String POWER_THERMAL_ALL = URI + "/all";
     
 }
